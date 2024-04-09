8005284083cfcf527f35289cbb5a931a67b0df28
karan <karanverma201411@gmail.com>
2024-04-09 23:40:18 +0530

    str to num and vice versa

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('welocome to JavaScript 2024');
+// console.log('welocome to JavaScript 2024');
+
+//convert string to a number
+let str = "4";
+// console.log(typeof +str);             first way
+// console.log(typeof Number(str));       second way
+
+//convert number to a string
+
+let num = 55;
+console.log(typeof num);
+// console.log(typeof num.toString());      first way
+// console.log(typeof String(num));        //second way
+console.log(typeof (num + ''));           //third way
