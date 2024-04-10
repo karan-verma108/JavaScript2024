31fccf6f474f0a52659f2e54f636fab546ef06ec
karan <karanverma201411@gmail.com>
2024-04-10 23:54:52 +0530

    paseint, parsefloat

--- index.js
+++ index.js
@@ -8,7 +8,16 @@ let str = "4";
 //convert number to a string
 
 let num = 55;
-console.log(typeof num);
+// console.log(typeof num);
 // console.log(typeof num.toString());      first way
 // console.log(typeof String(num));        //second way
-console.log(typeof (num + ''));           //third way
+// console.log(typeof (num + ''));           //third way
+
+//another way to convert string to number is using parseInt
+
+// let myStr = '43';
+// console.log(typeof parseInt(myStr));
+
+// but if we have something like this, then parseInt doesn't work as expected 
+let myStr = '43.6';
+console.log(parseFloat(myStr));         //then we use parseFloat
