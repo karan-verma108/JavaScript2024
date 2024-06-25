6285611ad0f16a06e0d749aad383380082e56dba
karan <karanverma201411@gmail.com>
2024-06-25 21:45:54 +0530

    added ex16 & ex17

---
+++ conditionalStatements/ex16.js
@@ -0,0 +1,19 @@
+// switch statement
+
+const day = 'Tuesday';
+
+switch (day) {
+  case 'Monday':
+    console.log('Yes it is monday');
+    break;
+  case 'Tuesday':
+    console.log('Yes it is tuesday');
+    break;
+  case 'Wednesday':
+    console.log('Yes it is wednesday');
+    break;
+
+  default:
+    console.log('Please enter a valid day');
+    break;
+}
---
+++ conditionalStatements/ex17.js
@@ -0,0 +1,32 @@
+// area of shapes using switch statement
+
+//area of triangle -> length * width
+//area of circle -> pie * (r*r)
+// area of square -> side * side
+
+const length = 5;
+const width = 6;
+const side = 9;
+const radius = 3;
+
+const areaOfShape = 'circle';
+
+switch (areaOfShape) {
+  case 'circle':
+    console.log(
+      `The area of circle is ${length} * ${width} = ${length * width}`
+    );
+    break;
+  case 'triangle':
+    console.log(
+      `The area of triange is ${Math.PI.toFixed(
+        2
+      )} * (${radius} * ${radius}) = ${(Math.PI * radius * radius).toFixed(2)}`
+    );
+    break;
+  case 'square':
+    console.log(`The area of square is ${side} * ${side} = ${side * side}`);
+    break;
+  default:
+    console.log('Please enter a recognizable shape');
+}
