7ff332aa553fd1a1c129a25a36a0e4a90ac0cada
karan <karanverma201411@gmail.com>
2024-09-27 23:35:42 +0530

    minor change in test file

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-console.log('this is a test file');
+// console.log('this is a test file');
+
+let score = '';
+
+let scoreInNum = Number(score);
+console.log('value of scoreInNum after conversion', scoreInNum);
+
+console.log('type of scoreInNum is', typeof scoreInNum);
