6e12de12810aea6b6050f3a9887264c07b44d2cc
karan <karanverma201411@gmail.com>
2024-10-17 06:50:57 +0530

    rest operator

--- basics/functions/index.js
+++ basics/functions/index.js
@@ -3,4 +3,21 @@ const userIsLogged = (userName = 'rocky') => {
 };
 
 const result = userIsLogged();
-console.log(result);
+// console.log(result);
+
+// use rest operator to recieve n number of arguments
+
+const calculateUserOrders = (...orderPrices) => {
+  return orderPrices.reduce((prevValue, currValue) => prevValue + currValue);
+};
+
+// console.log(calculateUserOrders(43, 55));
+
+const numberArr = [200, 400, 900, 500];
+
+const returnSecondValue = (numArr) => {
+  const [firstNum, secondNum, ...remainingNums] = numArr;
+  return secondNum;
+};
+
+console.log(returnSecondValue(numberArr));
