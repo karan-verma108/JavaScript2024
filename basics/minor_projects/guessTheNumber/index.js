a976eb97e741bbd53fe1c92f6e3f07d487f22f67
karan <karanverma201411@gmail.com>
2024-11-21 21:55:51 +0530

    guess the number

---
+++ basics/minor_projects/guessTheNumber/index.js
@@ -0,0 +1,33 @@
+const form = document.querySelector('form');
+const enteredNumRef = document.getElementById('enteredNum');
+const btn = document.getElementById('btn');
+
+let totalGuess = 10;
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const remainingGuesses = document.getElementById('remainingGuesses');
+  const previousGuesses = document.getElementById('previousGuesses');
+  const enteredNum = parseInt(document.getElementById('enteredNum').value);
+  const result = document.getElementById('result');
+  const randomNum = Math.floor(Math.random() * 10) + 1;
+
+  enteredNumRef.value = '';
+
+  if (totalGuess >= 1 && !isNaN(enteredNum)) {
+    if (enteredNum === randomNum) {
+      enteredNumRef.disabled = true;
+      btn.disabled = true;
+      const resultText = document.createTextNode(
+        `Congrats! you won!  Your guess = ${enteredNum} and random number = ${randomNum}`
+      );
+      result.appendChild(resultText);
+    } else {
+      result.innerHTML = '';
+      const previousGuessText = document.createTextNode(enteredNum);
+      previousGuesses.appendChild(previousGuessText);
+      previousGuesses.appendChild(document.createTextNode(','));
+    }
+    totalGuess--;
+    remainingGuesses.innerText = totalGuess;
+  }
+});
